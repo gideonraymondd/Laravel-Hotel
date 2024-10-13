648ee4d7ccac7fd9370c7ed1f3d9bd3a09997ba6
gideonraymondd <gideonraymondd@gmail.com>
2024-10-14 02:52:07 +0700

    feat ( dashboard controler) : add new function for card
    total booking, total order and total customer

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Room;
+use App\Models\Payment;
+use App\Models\Customer;
 use App\Models\Transaction;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -62,22 +64,7 @@ class DashboardController extends Controller
 
     public function index(Request $request)
 {
-    // Ambil tanggal dari input, gunakan hari ini jika tidak ada input
-    $date = $request->input('date', Carbon::now()->format('Y-m-d'));
-
-    // Mengubah input tanggal menjadi objek Carbon sesuai timezone Jakarta
-    $selectedDate = Carbon::createFromFormat('Y-m-d', $date, 'Asia/Jakarta');
-
-    // Ambil semua transaksi yang statusnya "reserved" pada tanggal yang dipilih
-    $occupiedRooms = Transaction::where('status', 'Reservation')
-        ->where(function ($query) use ($selectedDate) {
-            $query->whereDate('check_in', '<=', $selectedDate)
-                ->whereDate('check_out', '>=', $selectedDate);
-        })
-        ->pluck('room_id'); // Hanya ambil room_id yang sedang ditempati
-
-    // Ambil semua kamar dengan pagination
-    $allRooms = Room::simplePaginate(8); // 8 ruangan per halaman
+    //Check in check out dan cleaned by Date
 
     // Ambil filter dari query string, default ke 'check_in'
     $filter = $request->input('filter', 'check_in');
@@ -93,22 +80,47 @@ class DashboardController extends Controller
         $filterData = Transaction::whereDate('cleaned_time', $today)->get();
     }
 
+    // Room Status By Date
+
+    // Ambil tanggal dari input, gunakan hari ini jika tidak ada input
+    $date = $request->input('date', Carbon::now()->format('Y-m-d'));
+
+    // Mengubah input tanggal menjadi objek Carbon sesuai timezone Jakarta
+    $selectedDate = Carbon::createFromFormat('Y-m-d', $date, 'Asia/Jakarta');
+    // Ambil semua transaksi yang statusnya "reserved" pada tanggal yang dipilih
+    $occupiedRooms = Transaction::where('status', 'Reservation')
+        ->where(function ($query) use ($selectedDate) {
+            $query->whereDate('check_in', '<=', $selectedDate)
+                ->whereDate('check_out', '>=', $selectedDate);
+        })
+        ->pluck('room_id'); // Hanya ambil room_id yang sedang ditempati
+
+    // Ambil semua kamar dengan pagination
+    $allRooms = Room::simplePaginate(8); // 8 ruangan per halaman
+
+
+    // Reservation Pie chart
+
     // Ambil periode waktu dari request, default ke '1d'
     $timePeriod = $request->input('timePeriod', '1d');
     $dataForPieChart = ['checkin' => 0, 'checkout' => 0, 'reserved' => 0];
 
-
     // Hitung data berdasarkan periode yang dipilih
     if ($timePeriod === '1d') {
         // Ambil data untuk hari ini
-        $dataForPieChart['checkin'] = Transaction::whereDate('checked_in_time', now()->format('Y-m-d'))->count();
-        $dataForPieChart['checkout'] = Transaction::whereDate('checked_out_time', now()->format('Y-m-d'))->count();
-        $dataForPieChart['reserved'] = Transaction::whereDate('created_at', now()->format('Y-m-d'))->count();
+        $dataForPieChart['checkin'] = Transaction::whereDate('checked_in_time', now('Asia/Jakarta')->format('Y-m-d'))->count();
+        $dataForPieChart['checkout'] = Transaction::whereDate('checked_out_time', now('Asia/Jakarta')->format('Y-m-d'))->count();
+        $dataForPieChart['reserved'] = Transaction::whereDate('created_at', now('Asia/Jakarta')->format('Y-m-d'))->count();
+    } elseif ($timePeriod === '1w') {
+        // Ambil data untuk 1 minggu terakhir
+        $dataForPieChart['checkin'] = Transaction::whereBetween('checked_in_time', [now('Asia/Jakarta')->subDays(7), now('Asia/Jakarta')])->count();
+        $dataForPieChart['checkout'] = Transaction::whereBetween('checked_out_time', [now('Asia/Jakarta')->subDays(7), now('Asia/Jakarta')])->count();
+        $dataForPieChart['reserved'] = Transaction::whereBetween('created_at', [now('Asia/Jakarta')->subDays(7), now('Asia/Jakarta')])->count();
     } elseif ($timePeriod === '1m') {
         // Ambil data untuk bulan ini
-        $dataForPieChart['checkin'] = Transaction::whereMonth('checked_in_time', now()->month)->count();
-        $dataForPieChart['checkout'] = Transaction::whereMonth('checked_out_time', now()->month)->count();
-        $dataForPieChart['reserved'] = Transaction::whereMonth('created_at', now()->month)->count();
+        $dataForPieChart['checkin'] = Transaction::whereMonth('checked_in_time', now('Asia/Jakarta')->month)->count();
+        $dataForPieChart['checkout'] = Transaction::whereMonth('checked_out_time', now('Asia/Jakarta')->month)->count();
+        $dataForPieChart['reserved'] = Transaction::whereMonth('created_at', now('Asia/Jakarta')->month)->count();
     } elseif ($timePeriod === 'custom') {
         $startDate = $request->input('startDate');
         $endDate = $request->input('endDate');
@@ -116,8 +128,54 @@ class DashboardController extends Controller
         $dataForPieChart['checkin'] = Transaction::whereBetween('checked_in_time', [$startDate, $endDate])->count();
         $dataForPieChart['checkout'] = Transaction::whereBetween('checked_out_time', [$startDate, $endDate])->count();
         $dataForPieChart['reserved'] = Transaction::whereBetween('created_at', [$startDate, $endDate])->count();
-
     }
+
+    // Perbandingan Guest
+    // Mendapatkan tanggal untuk bulan ini dan bulan lalu
+    $thisMonth = Carbon::now()->month;
+    $lastMonth = Carbon::now()->subMonth()->month;
+
+    $thisYear = Carbon::now()->year;
+    $lastMonthYear = Carbon::now()->subMonth()->year;
+
+    // Jumlah order bulan ini
+    $ordersThisMonth = Transaction::whereMonth('created_at', $thisMonth)
+                            ->whereYear('created_at', $thisYear)
+                            ->count();
+
+    // Jumlah order bulan lalu
+    $ordersLastMonth = Transaction::whereMonth('created_at', $lastMonth)
+                            ->whereYear('created_at', $lastMonthYear)
+                            ->count();
+
+    // Menghitung selisih jumlah order
+    $difference = $ordersThisMonth - $ordersLastMonth;
+
+    // Card
+
+    // Ambil data bulan ini
+    $startOfMonth = Carbon::now()->startOfMonth();
+    $endOfMonth = Carbon::now()->endOfMonth();
+
+    // Ambil data bulan lalu
+    $startOfLastMonth = Carbon::now()->subMonth()->startOfMonth();
+    $endOfLastMonth = Carbon::now()->subMonth()->endOfMonth();
+
+    // Total Revenue
+    $totalRevenueThisMonth = Payment::whereBetween('created_at', [$startOfMonth, $endOfMonth])->sum('price');
+    $totalRevenueLastMonth = Payment::whereBetween('created_at', [$startOfLastMonth, $endOfLastMonth])->sum('price');
+    $revenueDifference = $totalRevenueThisMonth - $totalRevenueLastMonth; // Selisih Revenue
+
+    // Total Customer
+    $totalCustomersThisMonth = Customer::whereMonth('created_at', Carbon::now()->month)->count();
+    $totalCustomersLastMonth = Customer::whereMonth('created_at', Carbon::now()->subMonth()->month)->count();
+    $customersDifference = $totalCustomersThisMonth - $totalCustomersLastMonth; // Selisih Customer
+
+    // Total Booking
+    $totalBookingsThisMonth = Transaction::whereBetween('created_at', [$startOfMonth, $endOfMonth])->count();
+    $totalBookingsLastMonth = Transaction::whereBetween('created_at', [$startOfLastMonth, $endOfLastMonth])->count();
+    $bookingsDifference = $totalBookingsThisMonth - $totalBookingsLastMonth; // Selisih Booking
+
 
     // Kirim variabel transaksi dan kamar kosong ke view
     return view('dashboard.index', [
@@ -129,11 +187,23 @@ class DashboardController extends Controller
         'occupiedRooms' => $occupiedRooms, // Kamar yang sedang terisi
         'allRooms' => $allRooms, // Semua kamar
         'date' => $date, // Kirim tanggal ke view
-        'filter' => $filter, // Data check-in hari ini
-        'filterData' => $filterData, // Data check-out hari ini
-        'today' => $today, // Data cleaned hari ini
+        // 'filter' => $filter, // Data check-in hari ini
+        'filterData' => $filterData, // Data check-out,clean,checkin by date
+        // 'today' => $today, // Data cleaned hari ini
         'dataForPieChart' => $dataForPieChart, // Kirim data untuk chart ke view
         'timePeriod' => $timePeriod, // Kirim periode waktu ke view
+        // Perbanding Guest
+        'difference' => $difference,
+        // Card Total
+        'totalRevenueThisMonth' => $totalRevenueThisMonth,
+        'totalRevenueLastMonth' => $totalRevenueLastMonth,
+        'totalCustomersThisMonth' => $totalCustomersThisMonth,
+        'totalCustomersLastMonth' => $totalCustomersLastMonth,
+        'totalBookingsThisMonth' => $totalBookingsThisMonth,
+        'totalBookingsLastMonth' => $totalBookingsLastMonth,
+        'revenueDifference' => $revenueDifference,
+        'customersDifference' => $customersDifference,
+        'bookingsDifference' => $bookingsDifference,
     ]);
 }
 
